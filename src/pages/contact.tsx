675137877b504853e4a36668b7cf775f5c3c7b2c
Joel Rivera <pioneerwritings@gmail.com>
2021-02-02 17:00:25 -0500

    complete contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,30 @@
+import styles from 'styles/contact.module.scss'
 
 export default () => {
   return (
-    <div></div>
+    <div className={styles.contact}>
+      <header>
+        <img
+          src='/images/logo-dark.svg'
+          className={styles.logo}
+        />
+      </header>
+
+      <div className={styles.body}>
+        <h1>hello@joelrivera<span>.</span>me</h1>
+
+        <p>
+          Thank you for your interest in contacting me. If after reviewing my skillset,
+          experience, and portfolio, you feel that I would be the right candidate for an
+          interesting opportunity, please feel free to reach out to me, thanks!
+        </p>
+
+        <a href='mailto:hello@joelrivera.me'>
+          <button className={styles.button}>
+            <p>Get in touch</p>
+          </button>
+        </a>
+      </div>
+    </div>
   )
 }
