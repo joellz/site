40acb7596b65983d6600fa7b3732317e72b2c8c5
Joel Rivera <pioneerwritings@gmail.com>
2021-02-02 17:40:53 -0500

    add head for seo

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,10 +2,22 @@ import { motion } from 'framer-motion'
 
 import Link from 'next/link'
 import styles from 'styles/contact.module.scss'
+import Head from 'next/head'
 
 export default () => {
   return (
     <div className={styles.contact}>
+
+      <Head>
+        <title>Contact Me</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=no" />
+
+        <meta name="description" content="If after reviewing my skillset,
+          experience, and portfolio, you feel that I would be the right candidate for an
+          interesting opportunity, please feel free to reach out to me, thanks!"
+        />
+      </Head>
+
       <header>
         <Link href='/'>
           <img
